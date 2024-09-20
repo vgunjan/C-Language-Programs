b5f2f521bb149cf5370ca022e36d268160e47b6b
vgunjan <60270960+vgunjan@users.noreply.github.com>
2024-09-20 10:22:28 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d%d", &a,&b);
+    printf("value of a: %d \n", a);
+    printf("value of b: %d \n", b);
+    printf("Sum: %d", a+b);
+    return 0;
+}
